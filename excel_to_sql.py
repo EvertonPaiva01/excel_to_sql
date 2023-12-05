58d1747ba11e008c3ac01443a2b21a957106b8a6
Everton da Silva Paiva <evertonpaiva01@gmail.com>
2023-12-05 10:13:40 -0300

    Subindo arquivo de Script

---
+++ excel_to_sql.py
@@ -0,0 +1,39 @@
+#Importando bibliotecas
+import pandas as pd
+from datetime import date
+from sqlalchemy import create_engine
+import pymysql
+
+#Iniciando protocolo de comunicação com o MySQL
+pymysql.install_as_MySQLdb()
+
+#Carregando arquivos do excel
+arquivo = '_BaseContasAnalisadas.xlsx'
+pasta = 'C:/Users/Everton Paiva/Documents/Neoenergia/_BaseContasAnalisadas/'
+
+#Criando o DataFrame
+df = pd.read_excel(pasta+arquivo)
+
+#Função para se conectar ao banco de dados
+def mysql_connection(host, user, passwd, database=None):
+    engine = create_engine(f'mysql+pymysql://{user}:{passwd}@{host}/{database}')
+    return engine.connect()
+
+#Conexão como Banco de Dados
+connection = mysql_connection('localhost', 'root', '9236', 'db_tois_analisados')
+
+#Filtrando a tabela e selecionando apenas as colunas que serão utilizadas no banco de dados
+tabela_filtrada = df[['CONTA CONTRATO DA FATURA', 'TOI', 'INSTALAÇÃO', ' MONTANTE', 'STATUS DA ANALISE(ATESTADO)',
+       'Arquivo', 'Data Analise']].copy()
+
+#Atribuindo todos os filtros para a tabela final
+tabela = tabela_filtrada.copy()
+#Criando uma cópia da tabela com todos os filtros para ser adicionada ao banco de dados
+df1 = tabela.copy()
+
+#Redefinindo o nome das colunas
+df1.columns = ['ContaContratoFatura', 'TOI', 'Instalacao', 'Montante', 'StatusAnalise',
+       'Arquivo', 'DataAnalise']
+
+#Tranforando NaN para vazio e inserindo no banco de dados
+df1.fillna('').to_sql('tois',connection,if_exists='append',index=False)
